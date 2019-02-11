83edf7efefc8e18aa297e6376705aee02adceb1a
Krittawit <wernzkung@gmail.com>
2019-02-11 08:50:09 +0000

    first commit

---
+++ todo/urls.py
@@ -0,0 +1,18 @@
+#from django.urls import path
+
+#from . import views
+
+#urlpatterns = [
+#    path('', views.index, name='index'),
+#]
+from django.urls import path
+
+from . import views
+
+app_name = 'todo'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('<int:id>/delete', views.delete, name='delete'),
+]
